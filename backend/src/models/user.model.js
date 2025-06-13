84c1088b151c0f06d5fedb4a3ef8a94d2705b7ac
Ivanchik <iradolgaya88@icloud.com>
2025-06-14 00:35:03 +0400

    completed commit

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  profilePic:{
+    type: String,
+    default: "",
+  },
+
+ 
+},
+{
+  timestamps: true,
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
+// This code defines a Mongoose schema for a User model in a Node.js application.
